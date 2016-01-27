97b540112be55272dcb8aae3413cadf0b693a351
popfeng <popfeng@yeah.net>
2016-01-27 21:55:28 +0800

    Update: 小版本发布

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,7 +4,7 @@
 <div id="player"></div>
 <h1 class="post-title">关于</h1>
 <div class="post-about">
-    <p>你好！朋友！这里是飞鱼秀的非官方回放网站，赶紧扫描下方的二维码来拜见我们小人国的国主吧！</p>
+    <p>你好！朋友！这里是飞鱼秀的非官方回放网站，赶紧扫描下方的二维码来拜见我们的舟舟国主吧！</p>
     <div class="qrcode">
         <img src="/static/img/qrcode_zaoaoaoaoao.jpg">
         <img src="/static/img/qrcode_ezfeiyuxiu.jpg">
