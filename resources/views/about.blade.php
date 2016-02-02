24e0a04a7c8f9bf1b28e8164877d0b6b42f9d8de
popfeng <popfeng@yeah.net>
2016-02-02 18:33:11 +0800

    Update: 在关于页面添加参与规则

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,6 +10,17 @@
         <img src="/static/img/qrcode_ezfeiyuxiu.jpg">
     </div>
 </div>
+<span id="contribute" class="post-about-contribute">
+    由于待完善的节目话题和参与人信息数量巨大，单凭一己之力的话得做到猴年马月，所以还请我们飞鱼人一起完善。当你正在收听有空标题或空参与人的节目时，可以在下方回复：
+    <br>
+    <br>
+    🐶话题 🐶
+    <br>
+    🐰小飞|喻舟 🐰
+    <br>
+    <br>
+    用两只毛豆包住『话题』，用两只呆呆包住『参与人』，多个参与人用『 | 』分割，然后点击发布。此时我会收到一封邮件，几分钟后你所贡献的内容和花名就会出现在页面上啦~
+</span>
 @endsection
 
 @section('author')
