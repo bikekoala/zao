b6a4e9b544a071e43fa0ad4f4124d9aec9fcb194
popfeng <popfeng@yeah.net>
2016-08-04 11:27:42 +0800

    Update: 调整地图样式

--- public/static/js/here/map-base.js
+++ public/static/js/here/map-base.js
@@ -55,25 +55,6 @@ $.maps = (function() {
         mapData['data'].forEach(function (item, i) {
             series.push({
                 type: 'lines',
-                zlevel: 1,
-                effect: {
-                    show: true,
-                    period: 6,
-                    trailLength: 0.7,
-                    color: '#fff',
-                    symbolSize: 3
-                },
-                lineStyle: {
-                    normal: {
-                        color: color[i],
-                        width: 0,
-                        curveness: 0.2
-                    }
-                },
-                data: convertData(item[1])
-            },
-            {
-                type: 'lines',
                 zlevel: 2,
                 effect: {
                     show: true,
@@ -290,7 +271,9 @@ $.maps = (function() {
                                 }
                             },
                             tooltip : {
-                                show: false
+                                formatter: function(dataItem) {
+                                    return dataItem['data'][2] + ' 人次';
+                                }
                             },
                             data: data[1]
                         }
