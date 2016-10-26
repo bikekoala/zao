179f3dd2afa652b41bdb8431ac5d4b4c5890eb90
popfeng <popfeng@yeah.net>
2016-10-26 15:14:59 +0800

    Feature: Support simplified and traditional Chinese conversion

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -47,6 +47,8 @@
             </footer>
         </div>
 
+        <script src="/static/js/cn-tw.js"></script>
+
         <script type="text/javascript">
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
