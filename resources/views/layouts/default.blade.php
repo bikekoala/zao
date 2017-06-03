d88929c84bb1156114de3882f1e50f4072ebe9d8
popfeng <popfeng@yeah.net>
2017-06-03 11:50:29 +0800

    Update: switch to disqus

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -39,7 +39,7 @@
                     <a href="{{ URL('/') }}"><i class="fa fa-home" original-title="首页"></i></a>
                     <a href="{{ URL('music') }}"><i class="fa fa-music" original-title="歌曲"></i></a>
                     <!--a href="{{ URL('program/apptoday') }}"><i class="fa fa-calendar-check-o" original-title="APP 同期节目"></i></a-->
-                    <a href="{{ URL('here') }}"><i class="fa fa-map-o" original-title="签到"></i></a>
+                    <!--a href="{{ URL('here') }}"><i class="fa fa-map-o" original-title="签到"></i></a-->
                     <a href="{{ URL('about') }}"><i class="fa fa-about" original-title="关于"></i></a>
                 </nav>
 
--- resources/views/layouts/disqus.blade.php
+++ resources/views/layouts/disqus.blade.php
@@ -1,5 +1,5 @@
 <style type="text/css">
-    #disqus_thread {
+    #disqus_thread, #comment {
         margin-top:30px;
         width:auto;
         border-radius:5px;
@@ -7,15 +7,17 @@
     }
 </style>
 
-<div id="disqus_thread"></div>
+<div id="comment"></div>
+<script src="/disqus/disqus-api.js?shortname=zaoaoaoaoao"></script>
+<link rel="stylesheet" href="/disqus/disqus-api.css" />
 
-<script type="text/javascript">
+<!--div id="disqus_thread"></div-->
+<!--script type="text/javascript">
     (function() {
         var d = document, s = d.createElement('script');
         s.src = 'https://zaoaoaoaoao.disqus.com/embed.js';
         s.setAttribute('data-timestamp', +new Date());
         (d.head || d.body).appendChild(s);
     })();
-</script>
-
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+</script-->
+<!--noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript-->
