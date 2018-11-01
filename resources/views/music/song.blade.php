dd7ce51a5dd009a04b0d462f9e0d9aeb2cdd80d4
popfeng <popfeng@yeah.net>
2018-11-01 14:12:22 +0800

    Update: remove ACRCloud link

--- resources/views/music/song.blade.php
+++ resources/views/music/song.blade.php
@@ -56,7 +56,3 @@
 <link rel="stylesheet" href="/static/??css/music.css">
 <script src="/static/??module/echarts/echarts.min.js,js/music/chart.js,js/music/song.js"></script>
 @endsection
-
-@section('footer_extra')
-<a href="http://www.acrcloud.cn/music_scan_for_files_feiyuxiu" rel="nofollow" target="_blank" class="link">@音乐识别由 <span>ACRCloud</span> 提供</a>
-@endsection
