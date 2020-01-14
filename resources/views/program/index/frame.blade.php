dccc6362dea0773ffd6e6b3ffba87d223ec5891c
popfeng <popfeng@yeah.net>
2020-01-14 18:04:43 +0800

    添加备案信息

--- resources/views/program/index/frame.blade.php
+++ resources/views/program/index/frame.blade.php
@@ -13,3 +13,8 @@
 
 <script src="/static/??module/jquery/jquery.scrollspy.js,js/index.js"></script>
 @endsection
+
+@section('footer_extra')
+<br>
+<a href="http://www.beian.miit.gov.cn" rel="nofollow" target="_blank" class="link">京ICP备16005034号-1</a>
+@endsection
